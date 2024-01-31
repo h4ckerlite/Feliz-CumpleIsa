87968e8c79e5787015b3a64871fdd108332585a7
h4ckerlite <70925909+h4ckerlite@users.noreply.github.com>
2024-01-31 14:54:52 -0400

    HB 2024!!!

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: '27 DEC, 2023',
+  birthdate: '27 DEC, 2024',
   name: 'LUCIANA♥!'
 };
 
