75e3318d323b23fb11419abb1b95a51511496f7d
h4ckerlite <70925909+h4ckerlite@users.noreply.github.com>
2024-07-24 00:03:56 -0400

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: '6 MAR, 2024',
+  birthdate: '6 MAR, 2025',
   name: 'LUCIANA♥!'
 };
 
