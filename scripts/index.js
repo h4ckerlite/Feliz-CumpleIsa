c0ec13c3dde87dd6c3693dc1e023ae534561e659
h4ckerlite <70925909+h4ckerlite@users.noreply.github.com>
2024-07-24 00:02:50 -0400

    New Luu

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: '27 DEC, 2024',
+  birthdate: '6 MAR, 2024',
   name: 'LUCIANA♥!'
 };
 
